24bc6a231449793a91a6eaffa574c0e7b4b5aca1
Arjun Vasudevan <arjunsv@berkeley.edu>
2018-01-05 22:17:15 -0800

    changed to snake case

--- ml.py
+++ ml.py
@@ -9,19 +9,19 @@ def get_JSON(itemId):
 	json = requests.get("https://api.rsbuddy.com/grandExchange?a=guidePrice&i="+str(itemId)).json()
 	return json
 
-def get_current_selling_quantity(itemId):
+def get_current_selling_quantity(item_id):
 	return get_JSON(itemId)['sellingQuantity']
 
-def get_current_buying_quantity(itemId):
+def get_current_buying_quantity(item_id):
 	return get_JSON(itemId)['buyingQuantity']
 
-def get_current_selling_price(itemId):
+def get_current_selling_price(item_id):
 	return get_JSON(itemId)['selling']
 
-def get_current_buying_price(itemId):
+def get_current_buying_price(item_id):
 	return get_JSON(itemId)['buying']
 
-def get_current_overall_price(itemId):
+def get_current_overall_price(item_id):
 	return get_JSON(itemId)['overall']
 
 def get_current_population():
@@ -31,13 +31,20 @@ def get_current_population():
 	population_count = int(re.search('(\d+)',population_text).group(1))
 	return population_count
 
-def get_observation_vector(itemId):
+def get_observation_vector(item_id):
 	return [get_current_population(), get_current_selling_quantity(itemId), get_current_selling_price(itemId)]
 
 
-observations = []
+def get_training_data(item_id, interval, poll_period):
+	X = []
+	y = []
+	
+	t_middle = time.time() + 60 * interval / 2
+	t_end = time.time() + 60 * interval
+	while time.time() < t_middle:
+		time.sleep(poll_interval)
 
-def get_training_data(itemId, interval):
+
 	return
 
 
